fe23afba676ef26fbbc454982b7d3206f17504b8
Shi <weishi@microstrategy.com>
2018-06-12 09:50:19 +0800

    add requirement

--- manage.py
+++ manage.py
@@ -7,5 +7,5 @@ manager.add_command('runserver', Server(host='0.0.0.0', port=8080))
 
 
 if __name__ == '__main__':
-    manager.run()
-    # app.run(host='0.0.0.0', port=8080, debug=True)
+    # manager.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
